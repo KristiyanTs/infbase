34f615e7f4a7cdaf45632efcfa047bd79e359c11
Kyle Cooke <cookekyle99@yahoo.co.uk>
2019-02-01 23:44:21 +0000

    setup jwt based authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  #protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }
+
+  def render_resource(resource)
+    if resource.errors.empty?
+      render json: resource
+    else
+      validation_error(resource)
+    end
+  end
+
+  def validation_error(resource)
+    render json: {
+        errors: [
+            {
+                status: '400',
+                title: 'Bad Request',
+                detail: resource.errors,
+                code: '100'
+            }
+        ]
+    }, status: :bad_request
+  end
 end
