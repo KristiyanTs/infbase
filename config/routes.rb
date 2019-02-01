25e783d800f429a475fd9a8d69609932307feeec
Kyle Cooke <cookekyle99@yahoo.co.uk>
2019-02-01 23:44:21 +0000

    setup jwt based authorization

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   resources :teaching_sessions
   resources :course_members
   resources :courses
-  devise_for :users
+  devise_for :users, controllers:{
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#home"
 end
