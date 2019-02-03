fd61805f4c128a7d799a1333fa28c0b85d69a91a
Kyle Cooke <cookekyle99@yahoo.co.uk>
2019-02-03 20:17:22 +0000

    make logins persist between reloads and store login state in vuex

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
       sessions: 'users/sessions',
       registrations: 'users/registrations'
   }
+  scope '/users/:id' do
+    get '/courses', action: :courses, controller: 'users/related'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#home"
 end
