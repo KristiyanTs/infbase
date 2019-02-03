b926c79e7dfd82a0d026b80e6fb8c533b9d71c66
kristiyan <kristiyan.c@gmail.com>
2019-02-03 18:58:23 +0000

    Home page improvements

--- app/javascript/src/views/Landing.vue
+++ app/javascript/src/views/Landing.vue
@@ -52,16 +52,13 @@
                     <div class="row row-grid">
                         <div class="col-lg-4">
                             <card class="border-0" hover shadow body-classes="py-5">
-                                <icon name="ni ni-check-bold" type="primary" rounded class="mb-4">
-                                </icon>
-                                <h6 class="text-primary text-uppercase">Schedule</h6>
+                                <div class="text-center">
+                                  <icon name="ni ni-check-bold" type="primary" rounded class="mb-4">
+                                  </icon>
+                                  <h6 class="text-primary text-uppercase">Schedule</h6> 
+                                </div>
                                 <p class="description mt-3">Argon is a great free UI package based on Bootstrap 4
                                     that includes the most important components and features.</p>
-                                <div>
-                                    <badge type="primary" rounded>design</badge>
-                                    <badge type="primary" rounded>system</badge>
-                                    <badge type="primary" rounded>creative</badge>
-                                </div>
                                 <base-button tag="a" href="#" type="primary" class="mt-4">
                                     Learn more
                                 </base-button>
@@ -69,16 +66,13 @@
                         </div>
                         <div class="col-lg-4">
                             <card class="border-0" hover shadow body-classes="py-5">
-                                <icon name="ni ni-istanbul" type="success" rounded class="mb-4">
-                                </icon>
-                                <h6 class="text-success text-uppercase">FAQ</h6>
+                                <div class="text-center">
+                                  <icon name="ni ni-istanbul" type="success" rounded class="mb-4">
+                                  </icon>
+                                  <h6 class="text-success text-uppercase">FAQ</h6> 
+                                </div>
                                 <p class="description mt-3">Argon is a great free UI package based on Bootstrap
                                     4 that includes the most important components and features.</p>
-                                <div>
-                                    <badge type="success" rounded>business</badge>
-                                    <badge type="success" rounded>vision</badge>
-                                    <badge type="success" rounded>success</badge>
-                                </div>
                                 <base-button tag="a" href="#" type="success" class="mt-4">
                                     Learn more
                                 </base-button>
@@ -86,16 +80,13 @@
                         </div>
                         <div class="col-lg-4">
                             <card class="border-0" hover shadow body-classes="py-5">
-                                <icon name="ni ni-planet" type="warning" rounded class="mb-4">
-                                </icon>
-                                <h6 class="text-warning text-uppercase">Tutors</h6>
+                                <div class="text-center">
+                                  <icon name="ni ni-planet" type="warning" rounded class="mb-4">
+                                  </icon>
+                                  <h6 class="text-warning text-uppercase">Tutors</h6>
+                                </div>
                                 <p class="description mt-3">Argon is a great free UI package based on Bootstrap
                                     4 that includes the most important components and features.</p>
-                                <div>
-                                    <badge type="warning" rounded>marketing</badge>
-                                    <badge type="warning" rounded>product</badge>
-                                    <badge type="warning" rounded>launch</badge>
-                                </div>
                                 <base-button tag="a" href="#" type="warning" class="mt-4">
                                     Learn more
                                 </base-button>
@@ -107,7 +98,9 @@
         </div>
     </section>
     <section class="section section-lg">
+      <div class="container">
         <schedule></schedule>
+      </div>
     </section>
 
 
--- app/javascript/src/views/components/Schedule.vue
+++ app/javascript/src/views/components/Schedule.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		
+		<h1>Hey</h1>
 	</div>
 </template>
 
