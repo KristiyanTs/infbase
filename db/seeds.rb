fe9c6d6afd3a85dbea983f134250061ee8fec843
kristiyan <kristiyan.c@gmail.com>
2019-02-08 14:48:50 +0000

    Fix pagination

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+User.create! first_name: 'Ivan1', last_name: 'Petar1', role: 'student', email: 'john1@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+User.create! first_name: 'Ivan2', last_name: 'Petar2', role: 'student', email: 'john2@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+User.create! first_name: 'Ivan3', last_name: 'Petar3', role: 'student', email: 'john3@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+User.create! first_name: 'Ivan4', last_name: 'Petar4', role: 'student', email: 'john4@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+User.create! first_name: 'Ivan5', last_name: 'Petar5', role: 'student', email: 'john5@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+User.create! first_name: 'Ivan6', last_name: 'Petar6', role: 'student', email: 'john6@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+User.create! first_name: 'Ivan7', last_name: 'Petar7', role: 'student', email: 'john7@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+User.create! first_name: 'Ivan8', last_name: 'Petar8', role: 'student', email: 'john8@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+User.create! first_name: 'Ivan9', last_name: 'Petar9', role: 'tutor', email: 'john9@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+User.create! first_name: 'Ivan0', last_name: 'Petar0', role: 'tutor', email: 'john0@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+User.create! first_name: 'Ivan11', last_name: 'Petar11', role: 'tutor', email: 'john11@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+User.create! first_name: 'Ivan12', last_name: 'Petar12', role: 'tutor', email: 'john12@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+User.create! first_name: 'Ivan13', last_name: 'Petar13', role: 'admin', email: 'john13@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+User.create! first_name: 'Ivan14', last_name: 'Petar14', role: 'admin', email: 'john14@gmail.com', password: 'topsecret', password_confirmation: 'topsecret'
+
+Course.create! name: "CogSci"
+Course.create! name: "Inf2B"
+Course.create! name: "Inf2D"
+Course.create! name: "DA"
+Course.create! name: "OP"
