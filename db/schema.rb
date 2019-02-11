a885ee2755c8f1e6acaeac782b77ed425578d720
Kyle Cooke <cookekyle99@yahoo.co.uk>
2019-02-11 10:38:57 +0000

    remove course_id from topic and annotate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_205610) do
+ActiveRecord::Schema.define(version: 2019_02_11_103436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,10 +129,8 @@ ActiveRecord::Schema.define(version: 2019_02_09_205610) do
 
   create_table "topics", force: :cascade do |t|
     t.string "name"
-    t.bigint "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["course_id"], name: "index_topics_on_course_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -171,5 +169,4 @@ ActiveRecord::Schema.define(version: 2019_02_09_205610) do
   add_foreign_key "tags", "reports"
   add_foreign_key "tags", "topics"
   add_foreign_key "teaching_sessions", "users", column: "tutor_id"
-  add_foreign_key "topics", "courses"
 end
